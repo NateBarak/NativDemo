7527d5171d8792cab56a39493d356fe2659b1fd3
nativ barak <nativ@globekeeper.com>
2018-05-07 23:16:15 +0300

    Created LoginActivity Layout

---
+++ app/src/main/java/com/nativdemo/nativdemo/nativdemo/LoginActivity.java
@@ -0,0 +1,23 @@
+package com.nativdemo.nativdemo.nativdemo;
+
+import android.app.Activity;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.MotionEvent;
+import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
+import android.widget.Button;
+import android.widget.Toast;
+
+public class LoginActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+
+        setContentView(R.layout.activity_login);
+    }
+}
